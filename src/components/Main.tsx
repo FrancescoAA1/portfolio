69036c297c039162fb81676b6734e2cef25ea72b
FrancescoAA1 <francescofantin2002@gmail.com>
2026-05-03 00:16:58 +0200

    Update LinkedIn URL to correct profile

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -14,7 +14,7 @@ function Main() {
         <div className="content">
           <div className="social_icons">
             <a href="https://github.com/FrancescoAA1" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/francesco-abbamonte/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.linkedin.com/in/francesco-fantin-2819801a2/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Francesco Fantin</h1>
           <p>Software Engineer</p>
